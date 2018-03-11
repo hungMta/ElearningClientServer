9f506280b41b6d31301b1d26afd11e41f03475c3
hungMta <hung250596@gmail.com>
2018-03-11 20:17:47 +0700

    change port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('./lib/node_modules/express');
 var bodyParser = require('./lib/node_modules/body-parser');
-
+var PORT = process.env.PORT || 5000
 var courseController = require('./client/controller/coursecontroller/courseController');
 
 var app = express();
@@ -9,9 +9,9 @@ app.use(bodyParser.json({extended:false}))
 
 app.get('/courses',courseController.coursesList);
 
-var server = app.listen(8081,function(){
+var server = app.listen(PORT,function(){
     var host = server.address().host;
     var port = server.address().port;
 
-    console.log("Listening at http://%s:%s",host,port);
+    console.log("Listening at http://%s:%s",host,PORT);
 });
