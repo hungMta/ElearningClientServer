272a20c733b804d8fdaad06a3aeac0f9b1625f96
hungMta <hung250596@gmail.com>
2018-03-16 21:08:58 +0700

    enable all cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('./lib/node_modules/express');
 var bodyParser = require('./lib/node_modules/body-parser');
+var cors = require('./lib/node_modules/cors')
 var courseController = require('./client/controller/coursecontroller/courseController');
 var lessonController = require('./admin/controller/lessoncontroller/lessoncontroller');
 var userController = require('./client/controller/usercontroller/userController');
@@ -10,6 +11,8 @@ var adminController = require('./admin/controller/admincontroller/admincontrolle
 var courseAdminController = require('./admin/controller/coursecontroller/courseController');
 var PORT = process.env.PORT || 5000;
 var app = express();
+
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json({ extended: false }))
 
